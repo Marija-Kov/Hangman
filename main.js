23945dfd3d3afa60a511d8d48af1edbe73b0f852
Marija K <kccmrj@outlook.com>
2022-02-20 19:58:30 +0100

    Added .json to source words from,  styled empty char. fields

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
-let para = document.querySelector('p');
-let input = document.querySelector('input');
-let btn = document.querySelector('#btn');
 
 
-btn.addEventListener('click', async () => {
-try {
-    let word = input.innerText;
-    let URL = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-    let request = new Request(URL);
-    let response = await fetch(request);
-    let data = await response.text();
-    return console.log(data);
+let animals = fetch ('animals.json')
+              .then(response => response.json())
+              .then(data => getRandom(data))
+              .catch(err => console.log(err.message));
+             
+function getRandom(array) {
+        let animal = document.createElement('span');
+        let random = Math.floor( Math.random() * array.length);
+        animal.innerHTML = `${array[random]}`;
+        let letters = array[random].split('');
+        console.log(letters, letters.length);
 
-} catch(err){
-    console.log(err)
+        empties(letters)
+        
     }
-})
 
+function empties(chars) {
+    for (let i = 0; i < chars.length; i++){
+        let field = document.createElement('span');
+        field.classList.add(`${chars[i]}`);
+        //field.innerHTML = chars[i];
+        document.body.appendChild(field);
+    }
+}
