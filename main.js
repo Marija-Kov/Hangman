b38f44885d2c422c39e67db3803e45b70e3cb646
Marija K <kccmrj@outlook.com>
2022-02-21 11:50:18 +0100

    added qwerty.json and  function setKeys(keys)
    Added some corresponding CSS.

--- main.js
+++ main.js
@@ -4,13 +4,18 @@ let animals = fetch ('animals.json')
               .then(response => response.json())
               .then(data => getRandom(data))
               .catch(err => console.log(err.message));
+
+let qwerty = fetch ('qwerty.json')
+             .then(response => response.json())
+             .then(data => setKeys(data))
+             .catch(err => console.log(err.message));
              
 function getRandom(array) {
         let animal = document.createElement('span');
         let random = Math.floor( Math.random() * array.length);
         animal.innerHTML = `${array[random]}`;
         let letters = array[random].split('');
-        console.log(letters, letters.length);
+        console.log(letters);
 
         empties(letters)
         
@@ -22,5 +27,32 @@ function empties(chars) {
         field.classList.add(`${chars[i]}`);
         //field.innerHTML = chars[i];
         document.body.appendChild(field);
+    }
+}
+
+function setKeys(keys) {
+    for (let i = 0; i < keys.length; i++){
+    let key = document.createElement('span');
+    key.classList.add('key');
+    key.innerHTML = keys[i];
+    document.body.appendChild(key);
+    key.addEventListener('click', () => {
+        let miss = 0;
+        key.setAttribute('class', 'tried');
+        let guess = document.querySelectorAll('span');
+        guess.forEach(g => { 
+          if (g.classList == `${keys[i]}` || g.classList == `${keys[i].toUpperCase()}`){
+              key.innerHTML != g.classList ? g.innerHTML = keys[i].toUpperCase() : g.innerHTML = keys[i];
+              console.log(g.innerHTML + '--CORRECT')
+            } else if (g.classList == key.innerHTML ){ // needs another condition to isolate the case for incorrect!
+                console.log(g.innerHTML + '--incorrect')
+            }
+                       
+          
+           
+    })
+    
+    })
+    
     }
 }
