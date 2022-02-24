b90fa12346b7513d49a3d1057b59ee7445981ebb
Marija K <kccmrj@outlook.com>
2022-02-24 12:24:39 +0100

    body parts drawing on click, reloading after win/lose

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
 # Hangman
  A classic hangman game.
 
-1. Pick a random word 
-   -Find an API ex. animal names.
+DONE* 1. Pick a random word 
+   -Find an API ex. animal names, or create JSON to source words from
 
-2. Display corresponding number of empty character spaces
+DONE 2. Display corresponding number of empty character spaces
    -create array out of the string
    -string.length * span ; each span with [class = corresponding letter]
 
-3. Try letters clicking on alphabet table / keyup
+DONE 3. Try letters clicking on alphabet table / keyup
    -for every letter in the alphabet: addEventListener(click, step4()), 
-4. Mark letters tried
+DONE 4. Mark letters tried
    -change style in some way
    -go through the array from step 2. for the next 2 steps
-5. If word contains tried letter, fill the corresponding span with its id value (letter)
+DONE 5. If word contains tried letter, fill the corresponding span with its id value (letter)
    
-6. If word doesn't contain tried letter, add a bodyPart = [head, torso, arm1, arm2, leg1, leg2, eyes, mouth]
-  -use canvas API
+6. 
+  -on each miss, a different shape is drawn on canvas in a predefined order
 
-7. Game over when all body parts are drawn (i.e. after bodyPart.length number of misses)
+DONE 7. Game over when all body parts are drawn (i.e. after bodyPart.length number of misses) 
   
 
-8. Win when all empty char. spaces are filled
+DONE 8. Win when all empty char. spaces are filled
--- main.js
+++ main.js
@@ -1,6 +1,82 @@
 let miss = 0;
 let got = 0;
+let canvas = document.querySelector('#canvas');
+let ctx = canvas.getContext('2d');
 
+
+ctx.strokeStyle = 'grey';
+ctx.lineWidth = 3;
+
+let hanger = () => {
+ ctx.moveTo(30, 295);
+ ctx.lineTo(80, 295);
+ ctx.moveTo(55, 295);
+ ctx.lineTo(55, 20);
+ ctx.lineTo(150, 20);
+ ctx.lineTo(150, 75);
+ ctx.moveTo(55, 80);
+ ctx.lineTo(100 , 20)
+ ctx.stroke();
+ 
+}
+
+hanger();
+
+
+let head = () => {
+    ctx.arc(150, 100, 25, 0, 2 * Math.PI); 
+    ctx.stroke();
+}
+
+let torso = () => {
+    ctx.moveTo(150, 125);
+    ctx.lineTo(150, 190);
+    ctx.stroke();
+}
+
+let arm1 = () => {
+    ctx.moveTo(150, 140);
+    ctx.lineTo(180, 150);
+    ctx.stroke(); 
+}
+
+let arm2 = () => {
+    ctx.moveTo(150, 140);
+    ctx.lineTo(120, 150);
+    ctx.stroke(); 
+}
+
+let leg1 = () => {
+    ctx.moveTo(150, 190);
+    ctx.lineTo(180, 220);
+    ctx.stroke(); 
+}
+
+let leg2 = () => {
+    ctx.moveTo(150, 190);
+    ctx.lineTo(120, 220);
+    ctx.stroke(); 
+}
+
+let face = () => {
+    ctx.moveTo(140, 95);
+    ctx.lineTo(150, 105);
+    ctx.moveTo(150, 95);
+    ctx.lineTo(140, 105);
+    
+    ctx.moveTo(155, 95);
+    ctx.lineTo(165, 105);
+    ctx.moveTo(165, 95);
+    ctx.lineTo(155, 105);
+    
+    ctx.moveTo(145, 115);
+    ctx.quadraticCurveTo(155, 105, 165, 115);
+    ctx.stroke();
+}
+
+
+
+let bodyParts = [ '0', head, torso, arm1, arm2, leg1, leg2, face ];
 
 let animals = fetch ('animals.json')
               .then(response => response.json())
@@ -18,7 +94,6 @@ function getRandom(array) {
         animal.innerHTML = `${array[random]}`;
         let letters = array[random].split('');
         console.log(letters);
-
         empties(letters)
         
     }
@@ -27,10 +102,10 @@ function empties(chars) {
     for (let i = 0; i < chars.length; i++){
         let field = document.createElement('span');
         field.classList.add(`${chars[i]}`);
-        //field.innerHTML = chars[i];
         document.querySelector('#word').appendChild(field);
     }
 }
+
 
 function setKeys(keys) {
     for (let i = 0; i < keys.length; i++){
@@ -42,21 +117,26 @@ function setKeys(keys) {
         key.setAttribute('class', 'tried');
         key.setAttribute('disabled', 'true');
         let guess = document.querySelectorAll(`.${keys[i]} , .${keys[i].toUpperCase()}`);
-        //makes sure uppercase letter class is included
+        //   ^   makes sure uppercase letter class is included
          if ( guess.length === 0 ) { 
              miss++; 
-             miss === 7 ? console.log (`you lost !`) : console.log (`${miss} missed !`)
+             bodyParts[miss]();
+             miss === 7 ? console.log (`you lost !`) : console.log (`${miss} missed !`);
+             if (miss ===7) { setTimeout(() => { window.location.reload() }, 1000) }
         } else if ( guess.length != 0 ) {
             guess.forEach(g => {
                 g.classList.contains(`${keys[i].toUpperCase()}`) ? 
-                g.innerHTML = keys[i].toUpperCase() : g.innerHTML = keys[i]; //makes sure first letter is displayed in uppercase
-                
+                g.innerHTML = keys[i].toUpperCase() : g.innerHTML = keys[i]; 
+                //  ^  makes sure first letter is displayed in uppercase 
             })
             got += guess.length; console.log(`got ${got} !`) // divide guess.length by itself if multiple-letter guess is to be counted as 1 got
             let word = document.querySelectorAll('span');
             let str = '';
             word.forEach( w => { str += w.innerHTML });
-            if (got === word.length) { console.log(`yes, you guessed it: ${str} !`)} 
+            if ( got === word.length ) { 
+                console.log(`yes, you guessed it: ${str} !`)
+                setTimeout(() => { window.location.reload() }, 500);
+            } 
          }  
                             
     
