b7067cce590241339db13d447ef8bc22485e6820
Marija K <kccmrj@outlook.com>
2023-02-12 16:01:50 +0100

    style update + counter + refactoring

--- body_parts.js
+++ body_parts.js
@@ -2,74 +2,73 @@
 let canvas = document.querySelector('#canvas');
 let ctx = canvas.getContext('2d');
 
-
-ctx.strokeStyle = 'grey';
+ctx.strokeStyle = 'brown';
 ctx.lineWidth = 3;
 
 let hanger = () => {
  ctx.beginPath();   
- ctx.moveTo(30, 295);
+ ctx.moveTo(40, 295); 
  ctx.lineTo(80, 295);
- ctx.moveTo(55, 295);
- ctx.lineTo(55, 20);
- ctx.lineTo(150, 20);
- ctx.lineTo(150, 75);
- ctx.moveTo(55, 80);
- ctx.lineTo(100 , 20);
+ ctx.moveTo(55, 295); 
+ ctx.lineTo(55, 100); 
+ ctx.lineTo(150, 100);
+ ctx.lineTo(150, 125);
+ ctx.moveTo(55, 150);
+ ctx.lineTo(100 , 100);
  ctx.closePath();
  ctx.stroke();
  
 }
 let head = () => {
     ctx.beginPath(); 
-    ctx.arc(150, 100, 25, 0, 2 * Math.PI); 
+    ctx.arc(150, 140, 15, 0, 2 * Math.PI); 
     ctx.closePath();
     ctx.stroke();
 }
 
 let torso = () => {
-    ctx.moveTo(150, 125);
-    ctx.lineTo(150, 190);
+    ctx.moveTo(150, 153);
+    ctx.lineTo(150, 200);
     ctx.stroke();
 }
 
 let arm1 = () => {
-    ctx.moveTo(150, 140);
-    ctx.lineTo(180, 150);
+    ctx.moveTo(150, 170);
+    ctx.lineTo(180, 160);
     ctx.stroke(); 
 }
 
 let arm2 = () => {
-    ctx.moveTo(150, 140);
-    ctx.lineTo(120, 150);
+    ctx.moveTo(150, 170);
+    ctx.lineTo(120, 160);
     ctx.stroke(); 
 }
 
 let leg1 = () => {
-    ctx.moveTo(150, 190);
-    ctx.lineTo(180, 220);
+    ctx.moveTo(150, 200);
+    ctx.lineTo(180, 230);
     ctx.stroke(); 
 }
 
 let leg2 = () => {
-    ctx.moveTo(150, 190);
-    ctx.lineTo(120, 220);
+    ctx.moveTo(150, 200);
+    ctx.lineTo(120, 230);
     ctx.stroke(); 
 }
 
 let face = () => {
-    ctx.moveTo(140, 95);
-    ctx.lineTo(150, 105);
-    ctx.moveTo(150, 95);
-    ctx.lineTo(140, 105);
+    ctx.moveTo(145, 140);
+    ctx.lineTo(150, 135);
+    ctx.moveTo(150, 140);
+    ctx.lineTo(145, 135);
     
-    ctx.moveTo(155, 95);
-    ctx.lineTo(165, 105);
-    ctx.moveTo(165, 95);
-    ctx.lineTo(155, 105);
+    ctx.moveTo(155, 140);
+    ctx.lineTo(160, 135);
+    ctx.moveTo(160, 140);
+    ctx.lineTo(155, 135);
     
-    ctx.moveTo(145, 115);
-    ctx.quadraticCurveTo(155, 105, 165, 115);
+    ctx.moveTo(145, 147);
+    ctx.quadraticCurveTo(155, 143, 155, 149);
     ctx.stroke();
 }
 
--- main.js
+++ main.js
@@ -1,31 +1,117 @@
+import { hanger, head, torso, arm1, arm2, leg1, leg2, face } from '/body_parts.js';
+import {animals} from './animals.js'
+
+
+const bodyParts = [ '0', head, torso, arm1, arm2, leg1, leg2, face ];
+const keys = [
+      "q",
+      "w",
+      "e",
+      "r",
+      "t",
+      "y",
+      "u",
+      "i",
+      "o",
+      "p",
+      "a",
+      "s",
+      "d",
+      "f",
+      "g",
+      "h",
+      "j",
+      "k",
+      "l",
+      "z",
+      "x",
+      "c",
+      "v",
+      "b",
+      "n",
+      "m",
+];
 let miss = 0;
 let got = 0;
+const guessedP = document.querySelector('.words-guessed');
 
-import { hanger, head, torso, arm1, arm2, leg1, leg2, face } from '/body_parts.js';
+let guessed = localStorage.getItem('guessed');
+if(guessed){
+    guessedP.innerHTML = `Guessed words count: ${guessed}`;
+} else {
+    guessedP.innerHTML = `Guessed words count: 0`;
+}
+
+const reset = document.querySelector('.reset');
+reset.addEventListener('click', () => {
+    if (guessed){
+      localStorage.removeItem('guessed');
+      guessedP.innerHTML = `Guessed words count: 0`; 
+    } 
+})
+
+document.body.addEventListener("keyup", keyboardControl);
 
 hanger();
-
-let bodyParts = [ '0', head, torso, arm1, arm2, leg1, leg2, face ];
-
-let animals = fetch ('animals.json')
-              .then(response => response.json())
-              .then(data => getRandom(data))
-              .catch(err => console.log(err.message));
-
-let qwerty = fetch ('qwerty.json')
-             .then(response => response.json())
-             .then(data => setKeys(data))
-             .catch(err => console.log(err.message));
-             
-function getRandom(array) {
+setKeys();
+getRandom();
+           
+function getRandom() {
         let animal = document.createElement('span');
-        let random = Math.floor( Math.random() * array.length);
-        animal.innerHTML = `${array[random]}`;
-        let letters = array[random].split('');
-        console.log(letters);
+        let random = Math.floor( Math.random() * animals.length);
+        animal.innerHTML = `${animals[random]}`;
+        let letters = animals[random].split('');
         empties(letters)
         
     }
+
+function setKeys() {
+ for (let i = 0; i < keys.length; i++){
+      let keyEl = document.createElement('button');
+      keyEl.classList.add('key');
+      keyEl.innerHTML = keys[i];
+      document.querySelector('#keys').appendChild(keyEl);
+      keyEl.addEventListener('click', ()=>clicked(keyEl, i));
+  }
+
+}  
+
+function keyboardControl(ev) {
+ let char = ev.char || ev.charCode || ev.which;
+ let s = String.fromCharCode(char);
+ let keys = document.querySelectorAll('.key');
+ for (let i = 0; i < keys.length; i++){
+  if (keys[i].innerHTML === s || keys[i].innerHTML === s.toLowerCase()){
+      keys[i].click();
+  }
+ }  
+}
+
+function clicked(key, i) {
+ key.setAttribute('class', 'tried');
+ key.setAttribute('disabled', 'true');
+ let guess = document.querySelectorAll(`.${keys[i]} , .${keys[i].toUpperCase()}`);
+  if ( guess.length === 0 ) { 
+      miss++; 
+      bodyParts[miss]();
+      if (miss === bodyParts.length - 1) { setTimeout(() => { window.location.reload() }, 1000) }
+ } else if ( guess.length != 0 ) {
+     guess.forEach(g => {
+         g.classList.contains(`${keys[i].toUpperCase()}`) ? 
+         g.innerHTML = keys[i].toUpperCase() : g.innerHTML = keys[i]; 
+     })
+     got += guess.length; console.log(`got ${got} !`) // divide guess.length by itself if multiple-letter guess is to be counted as 1 got
+     let word = document.querySelectorAll('span');
+     let str = '';
+     word.forEach( w => { str += w.innerHTML });
+     if ( got === word.length ) { 
+         guessed++;
+         localStorage.setItem('guessed', guessed);
+         guessedP.innerHTML = `Guessed words count: ${guessed}`;
+         setTimeout(() => { window.location.reload() }, 1000);
+     } 
+   }                    
+ }  
 
 function empties(chars) {
     for (let i = 0; i < chars.length; i++){
@@ -37,59 +123,4 @@ function empties(chars) {
 
 
 
-function setKeys(keys) {
-    document.body.addEventListener('keyup', anyKey)
 
-    function anyKey (ev) {
-       // let target = ev.currentTarget;
-       // let tag = target.tagName;
-        let char = ev.char || ev.charCode || ev.which;
-        let s = String.fromCharCode(char);
-       // console.log(char, tag); console.log(s); 
-        let khees = document.querySelectorAll('.key');
-        for (let i = 0; i < khees.length; i++){
-            if (khees[i].innerHTML === s || khees[i].innerHTML === s.toLowerCase()){
-                khees[i].click();
-            }
-        }  
-    }
-
-    for (let i = 0; i < keys.length; i++){
-         let kiy = document.createElement('button');
-         kiy.classList.add('key');
-         kiy.innerHTML = keys[i];
-         document.querySelector('#keys').appendChild(kiy);
-         kiy.addEventListener('click', clicked);
-    
-    function clicked() {
-        kiy.setAttribute('class', 'tried');
-        kiy.setAttribute('disabled', 'true');
-        let guess = document.querySelectorAll(`.${keys[i]} , .${keys[i].toUpperCase()}`);
-        //   ^   makes sure uppercase letter class is included
-         if ( guess.length === 0 ) { 
-             miss++; 
-             bodyParts[miss]();
-             miss === bodyParts.length - 1 ? console.log (`you lost !`) : console.log (`${miss} missed !`);
-             if (miss === bodyParts.length - 1) { setTimeout(() => { window.location.reload() }, 1000) }
-        } else if ( guess.length != 0 ) {
-            guess.forEach(g => {
-                g.classList.contains(`${keys[i].toUpperCase()}`) ? 
-                g.innerHTML = keys[i].toUpperCase() : g.innerHTML = keys[i]; 
-                //  ^  makes sure first letter is displayed in uppercase 
-            })
-            got += guess.length; console.log(`got ${got} !`) // divide guess.length by itself if multiple-letter guess is to be counted as 1 got
-            let word = document.querySelectorAll('span');
-            let str = '';
-            word.forEach( w => { str += w.innerHTML });
-            if ( got === word.length ) { 
-                console.log(`yes, you guessed it: ${str} !`)
-                setTimeout(() => { window.location.reload() }, 500);
-            } 
-         }   
-        
-                          
-        }
-      
-
-}
-}
